05703181208154b9e67465484891449319f53f30
kevlawton <lawtonkevin@outlook.com>
2021-04-10 21:34:02 -0400

    working now

---
+++ index.js
@@ -0,0 +1,20 @@
+function getNoteId() {
+  let noteObject = getExistingNotes();
+  if (!noteObject) {
+    return 1;
+  }
+  const keysArray = Object.keys(noteObject);
+  const numberKeys = keysArray.map((key) => Number(key));
+  console.log(numberKeys);
+  let ret = Math.max(...numberKeys) + 1;
+  return ret;
+}
+function getExistingNotes() {
+  let notes = localStorage.getItem("notes");
+  if (!notes) {
+    return null;
+  }
+  return JSON.parse(notes);
+}
+
+getNoteId;
