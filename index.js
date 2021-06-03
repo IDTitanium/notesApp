56d6335d6ae8a2ca62b977c178303df71ad90ee7
darrianwesley <81720043+darrianwesley@users.noreply.github.com>
2021-06-03 07:56:18 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function getNoteId() {
+    let noteObject = getExistingNotes()
+    if(!noteObject){
+        return 1
+    }
+
+    const keysArray = Object.keys(noteObject);
+    const numberKeys = keysArray.map((key) => Number(key))
+    console.log(numberKeys);
+    return Math.max( ... numberKeys) + 1;
+}
+
+function getExistingNotes() {
+    let notes = localStorage.getItem('notes')
+    if(!notes){
+        return null
+    }
+    return JSON.parse(notes);
+}
